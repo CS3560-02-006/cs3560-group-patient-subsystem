d598754753fc27ac12adfac484ebe9bc54a21249
Joshua Reno <jralphur@gmail.com>
2023-03-13 18:31:42 -0700

    fix scripts

--- pull.sh
+++ pull.sh
@@ -2,4 +2,4 @@
 
 git subtree pull --prefix frontend https://github.com/CS3560-02-006/cs3560-patient-subsystem-frontend.git main --squash
 
-git subtree pull --prefix backend https://github.com/CS3560-02-006/cs3560-patient-subsystem-backend.git
+git subtree pull --prefix backend https://github.com/CS3560-02-006/cs3560-patient-subsystem-backend.git main --squash
--- push.sh
+++ push.sh
@@ -2,4 +2,4 @@
 
 git subtree push --prefix frontend https://github.com/CS3560-02-006/cs3560-patient-subsystem-frontend.git main
 
-git subtree push --prefix backend https://github.com/CS3560-02-006/cs3560-patient-subsystem-backend.git
+git subtree push --prefix backend https://github.com/CS3560-02-006/cs3560-patient-subsystem-backend.git main

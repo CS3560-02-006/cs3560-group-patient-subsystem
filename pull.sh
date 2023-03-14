7da32c3e82ee09b4ee00c3f7bb2c7d39ec154a40
Joshua Reno <jralphur@gmail.com>
2023-03-13 18:27:24 -0700

    migrate to submodules, add scripts

---
+++ pull.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git subtree pull --prefix frontend https://github.com/CS3560-02-006/cs3560-patient-subsystem-frontend.git main --squash
+
+git subtree pull --prefix backend https://github.com/CS3560-02-006/cs3560-patient-subsystem-backend.git
---
+++ push.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git subtree push --prefix frontend https://github.com/CS3560-02-006/cs3560-patient-subsystem-frontend.git main
+
+git subtree push --prefix backend https://github.com/CS3560-02-006/cs3560-patient-subsystem-backend.git

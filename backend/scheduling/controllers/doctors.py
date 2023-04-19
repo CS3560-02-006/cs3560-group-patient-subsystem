7b41ddca66dbdbd535989710ebcc8ede711f3382
BrendenJCPP <Brendenj14@gmail.com>
2023-04-19 11:37:51 -0700

    Doctors Controller, GET

--- backend/scheduling/controllers/doctors.py
+++ backend/scheduling/controllers/doctors.py
@@ -1,6 +1,11 @@
 from rest_framework.response import Response
+from scheduling.models import Doctor
+from scheduling.serializers import DoctorSerializer
+
 
 # fetches all doctors in database
 def getDoctors(request):
+    doctors = Doctor.objects.all()
+    serializer = DoctorSerializer(doctors, many =True)
  
-    return Response(request.data)
+    return Response(serializer.data)

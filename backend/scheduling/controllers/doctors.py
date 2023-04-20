ca4b920318cd79ae6ebf6a824eeaaa4fbd8ef6b6
BrendenJCPP <Brendenj14@gmail.com>
2023-04-20 08:48:43 -0700

    optimizing doctor api return for frontend

--- backend/scheduling/controllers/doctors.py
+++ backend/scheduling/controllers/doctors.py
@@ -14,5 +14,18 @@ def getDoctors(request):
                 lt.append((cursor.description[i][0], value))
         else:
             lt = dict(lt)
+            print(lt['doctorID'])
+            curs = connection.cursor()
+            curs.execute('select * from appointment where doctorID = {id}'.format(id=lt['doctorID']))
+            appointments = []
+            for row in curs:
+                appointment = []
+                for i, value in enumerate(row):
+                    if value != None:
+                        appointment.append((curs.description[i][0], value))
+                else:
+                    appointment = dict(appointment)
+                    appointments.append(appointment)
+            lt['appointments'] = appointments
             output.append(lt)
     return Response(output)

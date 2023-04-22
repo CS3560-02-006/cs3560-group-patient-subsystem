f832d7cbf2f28fd823780811c56bc50388f7fc5f
unknown <BrendenJCPP>
2023-04-22 16:45:12 -0700

    Mock data for usertype:patient

--- sql/GenerateDatabase.sql
+++ sql/GenerateDatabase.sql
@@ -1,12 +1,6 @@
--- MySQL Workbench Forward Engineering
-
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
-
--- -----------------------------------------------------
--- Schema appointmentsdb
--- -----------------------------------------------------
 
 -- -----------------------------------------------------
 -- Schema appointmentsdb
@@ -92,20 +86,20 @@ ENGINE = InnoDB;
 -- Table `appointmentsdb`.`User`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `appointmentsdb`.`User` (
-  `userID` INT NOT NULL AUTO_INCREMENT,
-  `email` VARCHAR(255) NOT NULL,
-  `password` VARCHAR(255) NOT NULL,
-  `userType` VARCHAR(45) NOT NULL,
-  `patientID` INT NULL,
-  PRIMARY KEY (`userID`),
-  INDEX `fk_User_Patient1_idx` (`patientID` ASC) VISIBLE,
-  CONSTRAINT `fk_User_Patient1`
-    FOREIGN KEY (`patientID`)
-    REFERENCES `appointmentsdb`.`Patient` (`patientID`)
+  userID INT NOT NULL AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL,
+  passwordHash VARBINARY(64) NOT NULL,
+  passwordSalt VARBINARY(32) NOT NULL,
+  userType VARCHAR(45) NOT NULL,
+  patientID INT NULL,
+  PRIMARY KEY (userID),
+  INDEX fk_User_Patient1_idx (patientID ASC) VISIBLE,
+  CONSTRAINT fk_User_Patient1
+    FOREIGN KEY (patientID)
+    REFERENCES appointmentsdb.Patient (patientID)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
+    ON UPDATE NO ACTION
+) ENGINE = InnoDB;
 
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
--- sql/init.sql
+++ sql/init.sql
@@ -133,15 +133,21 @@ INSERT INTO Appointment(doctorID, date, startTime, endTime, status)
     (1, "2023-05-09","12:30:00", "13:00:00", "available"),
     (1, "2023-05-10","13:30:00", "14:00:00", "available"),
     (2, "2023-05-10","11:00:00", "11:30:00", "available"),
-    (2, "2023-05-11","9:30:00", "10:00:00", "available"),
+    (2, "2023-05-11","9:30:00", "10:00:00", "available"), 
     (3, "2023-05-13","15:30:00", "16:00:00", "available"),
     (3, "2023-05-12","16:00:00", "16:30:00", "available")
 ;
 
-INSERT INTO appointmentsdb.User (email, passwordHash, passwordSalt, userType, patientID) VALUES (
-  'clerk@hospital.com', 
+INSERT INTO appointmentsdb.User (email, passwordHash, passwordSalt, userType, patientID) VALUES
+  ('clerk@hospital.com', 
   UNHEX('6c4f41c0f79f7d018087c335d82c9e4133d6df79531d4fece4ea4a71700148ec5425d00734959dfea70bc0b00a6bdc299340a8c6d845a7261dc34f4a3b41aba5'), 
   UNHEX('f33f81d480872fe6f0888fc0ee094d3e771365eec133476c7d2f58a769c7bbab'), 
   'clerk', 
   NULL
+  ),
+  ('user@hospital.com', 
+  UNHEX('c571304efbf9ef7128c2297bd05ccfb20571209823555f86ed6c33be96468a940ded36521e58775b6248ecc793cdd7974ca431d9067085bc53e4a6090bcec53b'), 
+  UNHEX('878c225be258f0e47bcd74e5231eb4931258cbdabb0cab0410e62fa1ad85d40e'), 
+  'patient', 
+  1
 );

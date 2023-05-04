06afa14226376950447efaacf2e9711c2380eb1c
BrendenJCPP <Brendenj14@gmail.com>
2023-05-04 16:05:45 -0700

    Finish try except blocks and email duplication check

--- backend/scheduling/controllers/auth.py
+++ backend/scheduling/controllers/auth.py
@@ -11,124 +11,159 @@ import json
 
 
 def getUser(user_id):
-    with connection.cursor() as cursor:
-        cursor.execute(
-            "SELECT userID, email, userType, patientID FROM appointmentsdb.User WHERE userID = %s", [user_id])
-        user = cursor.fetchone()
-        if user:
-            return {
-                'userID': user[0],
-                'email': user[1],
-                'userType': user[2],
-                'patientID': user[3],
-            }
-        else:
-            return None
+    try:
+        with connection.cursor() as cursor:
+            # Fetch user with the given user_id
+            cursor.execute(
+                "SELECT userID, email, userType, patientID FROM appointmentsdb.User WHERE userID = %s", [user_id])
+            user = cursor.fetchone()
+            if user:
+                return {
+                    'userID': user[0],
+                    'email': user[1],
+                    'userType': user[2],
+                    'patientID': user[3],
+                }
+            else:
+                return None
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 def createUser(request):
-    email = request.data.get('email')
-    password = request.data.get('password')
-    userType = request.data.get('userType')
-    patientID = request.data.get('patientID')
+    try:
+        email = request.data.get('email')
+        password = request.data.get('password')
+        userType = request.data.get('userType')
+        patientID = request.data.get('patientID')
 
-    salt = os.urandom(32)
-    password_hash = hashlib.pbkdf2_hmac(
-        'sha512', password.encode(), salt, 100000)
-
-    with connection.cursor() as cursor:
-        # Check if a user with the same patientID already exists
-        cursor.execute(
-            "SELECT * FROM appointmentsdb.User WHERE patientID = %s", [patientID])
-        existing_user = cursor.fetchone()
-
-        # If a user with the same patientID exists, return an error response
-        if existing_user:
-            return Response({"error": "A user with this patientID already exists"}, status=status.HTTP_400_BAD_REQUEST)
-
-        # If no such user exists, insert the new user record
-        cursor.execute("INSERT INTO appointmentsdb.User (email, passwordHash, passwordSalt, userType, patientID) VALUES (%s, %s, %s, %s, %s)", [
-                       email, password_hash, salt, userType, patientID])
-        user_id = cursor.lastrowid
-
-    return Response({"userID": user_id}, status=status.HTTP_201_CREATED)
-
-
-def updateUser(request, user_id):
-    email = request.data.get('email')
-    password = request.data.get('password')
-    userType = request.data.get('userType')
-    patientID = request.data.get('patientID')
-
-    update_fields = {}
-    if email:
-        update_fields['email'] = email
-    if userType:
-        update_fields['userType'] = userType
-    if patientID is not None:
-        update_fields['patientID'] = patientID
-
-    with connection.cursor() as cursor:
-        if password:
-            cursor.execute(
-                "SELECT passwordSalt FROM appointmentsdb.User WHERE userID=%s", [user_id])
-            salt = cursor.fetchone()[0]
-            password_hash = hashlib.pbkdf2_hmac(
-                'sha512', password.encode(), salt, 100000)
-            update_fields['passwordHash'] = password_hash
-
-        if update_fields:
-            set_clause = ', '.join(
-                [f"{field}=%s" for field in update_fields.keys()])
-            set_values = list(update_fields.values())
-            set_values.append(user_id)
-            cursor.execute(
-                f"UPDATE appointmentsdb.User SET {set_clause} WHERE userID=%s", set_values)
-
-    return Response(status=status.HTTP_200_OK)
-
-
-def deleteUser(user_id):
-    with connection.cursor() as cursor:
-        cursor.execute(
-            "DELETE FROM appointmentsdb.User WHERE userID = %s", [user_id])
-        return cursor.rowcount > 0
-
-
-def loginUser(request):
-    email = request.data.get('email')
-    password = request.data.get('password')
-
-    cursor = connection.cursor()
-    cursor.execute(
-        "SELECT userID, passwordHash, passwordSalt, userType, patientID FROM appointmentsdb.User WHERE email = %s", [email])
-    user = cursor.fetchone()
-
-    if user:
-        user_id, stored_hash, salt, user_type, patient_id = user
+        # Generate salt and hash the password
+        salt = os.urandom(32)
         password_hash = hashlib.pbkdf2_hmac(
             'sha512', password.encode(), salt, 100000)
 
-        if password_hash == stored_hash:
-            token_payload = generate_token(user_id, email, user_type)
-            token = encode_token(token_payload)
-            response = {
-                'userID': user_id,
-                'email': email,
-                'userType': user_type,
-                'patientID': patient_id,
-                'token': token,
-            }
-            return Response(response, status=status.HTTP_200_OK)
+        with connection.cursor() as cursor:
+            # Check if a user with the same email already exists
+            cursor.execute(
+                "SELECT * FROM appointmentsdb.User WHERE email = %s", [email])
+            existing_email_user = cursor.fetchone()
+
+            # If a user with the same email exists, return an error response
+            if existing_email_user:
+                return Response({"error": "A user with this email already exists"}, status=status.HTTP_400_BAD_REQUEST)
+
+            # Check if a user with the same patientID already exists
+            cursor.execute(
+                "SELECT * FROM appointmentsdb.User WHERE patientID = %s", [patientID])
+            existing_user = cursor.fetchone()
+
+            # If a user with the same patientID exists, return an error response
+            if existing_user:
+                return Response({"error": "A user with this patientID already exists"}, status=status.HTTP_400_BAD_REQUEST)
+
+            # If no such user exists, insert the new user record
+            cursor.execute("INSERT INTO appointmentsdb.User (email, passwordHash, passwordSalt, userType, patientID) VALUES (%s, %s, %s, %s, %s)", [
+                           email, password_hash, salt, userType, patientID])
+            user_id = cursor.lastrowid
+
+        return Response({"userID": user_id}, status=status.HTTP_201_CREATED)
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+def updateUser(request, user_id):
+    try:
+        email = request.data.get('email')
+        password = request.data.get('password')
+        userType = request.data.get('userType')
+        patientID = request.data.get('patientID')
+
+        update_fields = {}
+        if email:
+            update_fields['email'] = email
+        if userType:
+            update_fields['userType'] = userType
+        if patientID is not None:
+            update_fields['patientID'] = patientID
+
+        with connection.cursor() as cursor:
+            if password:
+                # Get salt and hash the new password
+                cursor.execute(
+                    "SELECT passwordSalt FROM appointmentsdb.User WHERE userID=%s", [user_id])
+                salt = cursor.fetchone()[0]
+                password_hash = hashlib.pbkdf2_hmac(
+                    'sha512', password.encode(), salt, 100000)
+                update_fields['passwordHash'] = password_hash
+
+            if update_fields:
+                # Update fields with new values
+                set_clause = ', '.join(
+                    [f"{field}=%s" for field in update_fields.keys()])
+                set_values = list(update_fields.values())
+                set_values.append(user_id)
+                cursor.execute(
+                    f"UPDATE appointmentsdb.User SET {set_clause} WHERE userID=%s", set_values)
+
+        return Response(status=status.HTTP_200_OK)
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+def deleteUser(user_id):
+    try:
+        with connection.cursor() as cursor:
+            # Execute delete query for the user with the given user_id
+            cursor.execute(
+                "DELETE FROM appointmentsdb.User WHERE userID = %s", [user_id])
+            
+            # Check if any rows were affected by the query
+            return cursor.rowcount > 0
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+def loginUser(request):
+    try:
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        cursor = connection.cursor()
+        # Fetch user with the given email
+        cursor.execute(
+            "SELECT userID, passwordHash, passwordSalt, userType, patientID FROM appointmentsdb.User WHERE email = %s", [email])
+        user = cursor.fetchone()
+
+        if user:
+            user_id, stored_hash, salt, user_type, patient_id = user
+            # Hash the input password with the stored salt
+            password_hash = hashlib.pbkdf2_hmac(
+                'sha512', password.encode(), salt, 100000)
+
+            # Compare the input password hash with the stored hash
+            if password_hash == stored_hash:
+                # Generate and encode the JWT token
+                token_payload = generate_token(user_id, email, user_type)
+                token = encode_token(token_payload)
+                response = {
+                    'userID': user_id,
+                    'email': email,
+                    'userType': user_type,
+                    'patientID': patient_id,
+                    'token': token,
+                }
+                return Response(response, status=status.HTTP_200_OK)
+            else:
+                return Response({"error": "Invalid email or password"}, status=status.HTTP_401_UNAUTHORIZED)
         else:
             return Response({"error": "Invalid email or password"}, status=status.HTTP_401_UNAUTHORIZED)
-    else:
-        return Response({"error": "Invalid email or password"}, status=status.HTTP_401_UNAUTHORIZED)
-
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 def generate_token(user_id, email, user_type):
-    # Generate the token payload
+    # Set token expiration to 7 days from now
     exp = datetime.utcnow() + timedelta(days=7)
+    
+    # Create the token payload
     payload = {
         'sub': user_id,
         'email': email,
@@ -137,21 +172,25 @@ def generate_token(user_id, email, user_type):
     }
     return payload
 
-
 def encode_token(payload):
-    # Encode the payload as a JWT token
+    # Define the JWT header
     header = {'typ': 'JWT', 'alg': 'HS256'}
     header_json = json.dumps(header, separators=(',', ':'))
     payload_json = json.dumps(payload, separators=(',', ':'))
 
+    # Base64 encode the header and payload
     header_b64 = base64.urlsafe_b64encode(
         header_json.encode('utf-8')).decode('utf-8').rstrip('=')
     payload_b64 = base64.urlsafe_b64encode(
         payload_json.encode('utf-8')).decode('utf-8').rstrip('=')
 
+    # Create the token signature using the SECRET_KEY
     signature = hmac.new(settings.SECRET_KEY.encode(
         'utf-8'), f'{header_b64}.{payload_b64}'.encode('utf-8'), hashlib.sha256).digest()
+    
+    # Base64 encode the signature
     signature_b64 = base64.urlsafe_b64encode(
         signature).decode('utf-8').rstrip('=')
 
+    # Return the complete JWT token
     return f'{header_b64}.{payload_b64}.{signature_b64}'
--- backend/scheduling/controllers/doctors.py
+++ backend/scheduling/controllers/doctors.py
@@ -1,56 +1,70 @@
 from rest_framework.response import Response
+from rest_framework import status
 from django.db import connection
 
 
-# fetches all doctors in database
+# Fetches all doctors in the database or a single doctor by ID
 def getDoctors(request, doctor_id=None):
     """
     Get all doctors or a single doctor by ID along with their associated appointments
     """
-    with connection.cursor() as cursor:
-        if doctor_id:
-            cursor.execute("""
-                SELECT Doctor.*, Appointment.*
-                FROM Doctor
-                LEFT JOIN Appointment ON Doctor.doctorID = Appointment.doctorID
-                WHERE Doctor.doctorID = %s
-            """, [doctor_id])
-        else:
-            cursor.execute("""
-                SELECT Doctor.*, Appointment.*
-                FROM Doctor
-                LEFT JOIN Appointment ON Doctor.doctorID = Appointment.doctorID
-            """)
+    try:
+        # Connect to the database
+        with connection.cursor() as cursor:
+            # If doctor_id is provided, fetch the doctor with the given ID
+            if doctor_id:
+                cursor.execute("""
+                    SELECT Doctor.*, Appointment.*
+                    FROM Doctor
+                    LEFT JOIN Appointment ON Doctor.doctorID = Appointment.doctorID
+                    WHERE Doctor.doctorID = %s
+                """, [doctor_id])
+            # If doctor_id is not provided, fetch all doctors
+            else:
+                cursor.execute("""
+                    SELECT Doctor.*, Appointment.*
+                    FROM Doctor
+                    LEFT JOIN Appointment ON Doctor.doctorID = Appointment.doctorID
+                """)
 
-        result = [dict(zip([column[0] for column in cursor.description], row))
-                  for row in cursor.fetchall()]
+            # Fetch the results and convert them to dictionaries
+            result = [dict(zip([column[0] for column in cursor.description], row))
+                      for row in cursor.fetchall()]
 
-    # Map the result to match the frontend interface
-    doctors = {}
-    for row in result:
-        doctorID = row['doctorID']
-        if doctorID not in doctors:
-            doctors[doctorID] = {
-                'doctorID': doctorID,
-                'name': row['name'],
-                'dateofBirth': row['dateofBirth'].strftime('%Y-%m-%d'),
-                'specialty': row['specialty'],
-                'appointments': [],
-            }
+        # Map the result to match the frontend interface
+        doctors = {}
+        for row in result:
+            doctorID = row['doctorID']
+            # If the doctor is not in the doctors dictionary, add them
+            if doctorID not in doctors:
+                doctors[doctorID] = {
+                    'doctorID': doctorID,
+                    'name': row['name'],
+                    'dateofBirth': row['dateofBirth'].strftime('%Y-%m-%d'),
+                    'specialty': row['specialty'],
+                    'appointments': [],
+                }
 
-        if row['appointmentID'] is not None:
-            appointment = {
-                'appointmentID': row['appointmentID'],
-                'doctorID': doctorID,
-                'patientID': row['patientID'],
-                'date': row['date'].strftime('%Y-%m-%d'),
-                'startTime': row['startTime'].strftime('%H:%M:%S'),
-                'endTime': row['endTime'].strftime('%H:%M:%S'),
-                'status': row['status'],
-            }
+            # If the appointmentID is not None, append the appointment to the doctor's appointments list
+            if row['appointmentID'] is not None:
+                appointment = {
+                    'appointmentID': row['appointmentID'],
+                    'doctorID': doctorID,
+                    'patientID': row['patientID'],
+                    'date': row['date'].strftime('%Y-%m-%d'),
+                    'startTime': row['startTime'].strftime('%H:%M:%S'),
+                    'endTime': row['endTime'].strftime('%H:%M:%S'),
+                    'status': row['status'],
+                }
 
-            doctors[doctorID]['appointments'].append(appointment)
+                doctors[doctorID]['appointments'].append(appointment)
 
-    formatted_doctors = list(doctors.values())
+        # Format the doctors as a list
+        formatted_doctors = list(doctors.values())
 
-    return Response(formatted_doctors)
+        # Return the formatted doctors with a 200 OK status
+        return Response(formatted_doctors, status=status.HTTP_200_OK)
+
+    except Exception as e:
+        # If an error occurs, return a 500 Internal Server Error status along with the error message
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
